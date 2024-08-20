8584726acf7a863805f7c314475483b1f5a93669
nicholasmorlin <nicholas_morlin@hotmail.com>
2024-08-19 21:14:58 -0300

    initiating project

---
+++ src/test/java/com/caju/CajuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
